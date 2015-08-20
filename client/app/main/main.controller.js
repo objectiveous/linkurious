1c3a1f388511eada723c416ec158132a1a8fc782
unknown <HWILKHU@LUK-PB7YWFL.corp.capgemini.com>
2015-08-20 12:51:29 +0100

    first commit
    no changes

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('linkuriousApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
---
+++ client/app/main/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('linkuriousApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
+  
+ 
